53964bea3a5afd99b6b6147985b8c9221d18e825
ericktorres1 <ericktorres90.et@gmail.com>
2015-03-31 19:02:02 -0400

    Initial Commit

---
+++ CoreImageFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreImageFun
+//
+//  Created by Erick Torres on 3/31/15.
+//  Copyright (c) 2015 Erick Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreImageFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreImageFun
+//
+//  Created by Erick Torres on 3/31/15.
+//  Copyright (c) 2015 Erick Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
